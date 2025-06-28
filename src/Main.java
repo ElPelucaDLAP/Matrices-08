09bf1592219886043f9fd1347758b61fec9effe9
ElPeluc4 <agusnarsaniga@proton.me>
2025-06-28 02:18:40 -0300

    1
    a

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] matriz ={
+                {1,0,2,3,4,0,0,2,4,0},
+                {0,3,2,4,0,0,0,2,1,0},
+                {0,2,3,2,0,2,2,1,0,0},
+                {0,3,4,2,0,9,2,1,0,0},
+                {0,2,2,2,2,2,2,0,0,0},
+        };
+        final int MAXF = 5;
+        final int MAXC = 10;
+    }
+}
